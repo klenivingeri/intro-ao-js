a88c593029c992bb7153c5f6f527fb87b2017ceb
Erick <libertti@live.com>
2021-02-21 21:21:15 -0300

    Exercicio 14

--- 14exer - Descontruir/main.js
+++ 14exer - Descontruir/main.js
@@ -1,2 +1,44 @@
+// Descontruindo Objeto
+
+console.log("Descontruindo Objeto")
+const pessoa ={
+    nome: "Erick",
+    sobrenome: "Kleniving",
+    idade: 28,
+    cargo: "Desenvolvedor"
+}
+
+console.log(pessoa)
+
+let { nome, idade } =  pessoa
+
+console.log(nome)
+console.log(idade)
+
+let { cargo:desenvolvedor } = pessoa
 
 
+console.log(desenvolvedor)
+
+// Descontruindo Array
+
+ let pessoas = ["Erick", "Kleniving", 28, "Desenvolvedor"]
+
+ console.log("Descontruindo Array")
+
+ let { 0:primeironome, 1:segundonome } = pessoas
+
+ console.log(primeironome, segundonome)
+
+
+
+ // Descontruindo Array 2º
+
+
+ let cadastro = ["Erick", "Kleniving", 28, "Desenvolvedor"]
+
+
+ let [ c_nome, c_sobrenome, c_idade ] = cadastro
+
+
+ console.log(c_nome, c_sobrenome, c_idade)
