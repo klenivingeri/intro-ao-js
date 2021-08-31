29a537b1320a1aad3c20df4d0e8bc0dc31b642a2
Erick Kleniving <libertti@live.com>
2021-08-31 19:11:58 -0300

    Metaprogramação e Proxy

--- 49exer-MetaprogramacaoProxies/main.js
+++ 49exer-MetaprogramacaoProxies/main.js
@@ -1,58 +1,45 @@
-console.log('--------------Exemplo 1')
-let promise1 = new Promise((resolve, reject)=>{
-  // corpo da promisse
-})
-promise1.then();
-promise1.catch()
+class Usuario {
+  constructor(login, senha){
+    this.login = login; 
+    this.senha = senha;
+  }
+}
+const usuario = new Usuario('SuperJS','123');
+console.log(usuario.login);// SuperJS
+console.log(usuario.senha);// 123
 
-console.log('--------------Exemplo 2')
-let promise2 = new Promise((resolve, reject)=>{
-  let resultado  = true
-  if(resultado == false){
-    resolve('Promisse resolvida')
-  }else{
-    reject('Promisse não resolvida')
+// exemplo1
+const proxie = new Proxy(usuario, {
+  get(alvo, propriedade){
+    console.log(`${propriedade} foi solicitada!`)
+    return alvo[propriedade]
   }
 })
-promise2.then( data =>{
-    console.log(data)
-}).catch( data =>{
-  console.log(data)
-})
-/**
-console.log('--------------Exemplo 3')
-let promise3 = new Promise((resolve, reject)=>{
-  let resultado  = true
-  let time = 2000
-  setTimeout(()=>{
-    if(resultado == true){
-      resolve('Promisse resolvida')
-    }else{
-      reject('Promisse não resolvida')
+
+console.log(proxie.login)
+console.log(proxie.senha)
+
+// exemplo2
+const validador = {
+  set(alvo, propriedade, valor){
+    if(propriedade == 'idade'){
+      if(!Number.isInteger(valor)){
+        throw new Error('A idade não é um número!')
+      }
     }
-  },time)
-}).then( data =>{ console.log(data); return data})
-  .then( data =>{ console.log(data, '2º then')})
-  .catch( data =>{ console.log(data)})
-*/
+    return alvo[propriedade] = valor
+  }
+}
+const user = new Proxy({},validador);
 
-console.log('--------------Exemplo 4')
-let promise4 = new Promise((resolve, reject)=>{
- throw new Error('error!')
- resolve('ok')
-}).then( data =>{ console.log('sucesso',data)})
-  .catch( data =>{ console.log('falha', data)})
-  
 
-let promise5 = new Promise((resolve, reject) =>{
-  resolve('ok1')
-}).then( data=>{
-  console.log(data);
+console.log(user.idade = 29)
 
-  throw new Error('Deu ruim')
-  return data;
-}).then(data =>{
-  console.log(data)
-}).catch(data =>{
-  console.log('Falho', data)
-})
+//exemplo3 
+
+const {proxy, revoke} = Proxy.revocable({}, {});
+proxy.test = 'haha'
+console.log(proxy.test)
+
+revoke() //remove a proxy
+console.log(proxy.test)
